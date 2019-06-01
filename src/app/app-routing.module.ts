e04c9036306466084d5241ad7f8ca46c38539155
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-06-01 16:56:10 -0500

    feat(lazy): Redirect home from ''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: 'samples', loadChildren: './pages/home/home-page-module.module#HomePageModuleModule' },
   { path: 'interceptors', loadChildren: './pages/repositories/repositories-module.module#RepositoriesModule' },
   { path: 'shortener', loadChildren: './pages/shortener/shortener.module#ShortenerModule' },
+  { path: '', redirectTo: 'samples', pathMatch: 'full' },
   { path: '**', redirectTo: '/samples', pathMatch: 'full' }
 ];
 
